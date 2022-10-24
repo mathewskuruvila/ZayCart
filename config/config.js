e2abb4bf912efed77e9dbc0795eaf2b08cd94e19
mathews <mathewsulike20@gmail.com>
2022-10-24 19:57:31 +0530

    local server created

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports={
+
+    ServiceID:"VA497c707757353fad83f2c92e5bc50c2e",
+    accountSID:"AC8da1a317502c379b7ea9810e08509ec9",
+    authTocken:"2d40528511aaa9fc9f0b931b702ce8f8"
+
+}
+
